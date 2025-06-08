566d4a2d5771e09e5e4d5662c5a7788f1ffa492d
satyendragautam90 <satyendragautam901@gmail.com>
2025-06-08 23:46:05 +0530

    Implement Sum of Each Row in a 2D Array

---
+++ TwoDArray/Matrix/SumOfEachRow.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int ar[4][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+    int ar2[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  return 0;
+}

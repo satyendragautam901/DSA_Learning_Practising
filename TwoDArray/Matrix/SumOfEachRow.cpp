5f8793d828d0793d03071d5e1e5c0d7f2726e988
satyendragautam90 <satyendragautam901@gmail.com>
2025-06-08 23:53:17 +0530

    SumOfEachRow in a 2D Array

--- TwoDArray/Matrix/SumOfEachRow.cpp
+++ TwoDArray/Matrix/SumOfEachRow.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
 using namespace std;
+void SumOfRow(int ar[][4], int row, int col)
+{
+    
+    // Row Sum
+    cout << "Row-wise Sum:"<<endl;
+    
+    // This loop iterate over each row
+    for (int i = 0; i < row; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < col; j++)
+            sum += ar[i][j];
+        cout << "Row " << i + 1 << ": " << sum << endl;
+    }
+
+    
+}
 int main()
 {
     int ar[4][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-    int ar2[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    SumOfRow(ar,4,4);
   return 0;
 }

91245316245226f5210bc6f2b3ba390eab2c9a63
Satyendra Gautam <142148319+satyendragautam901@users.noreply.github.com>
2025-06-13 19:16:41 +0530

    Sliding window with more documented form (#15)

--- NewString/Problems/SlidingWindow.cpp
+++ NewString/Problems/SlidingWindow.cpp
@@ -1,27 +1,33 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-void SlidingWindow(string str){
-    vector<bool>count(256,0);
+void SlidingWindow(string str) {
+    vector<bool> count(256, 0);
     
-    int first = 0, second =0, len = 0;
+    int first = 0, second = 0, len = 0;
 
-    while (second <str.size())
-    {
-        while (count[str[second]]) // repeating character 
-        {
+    while (second < str.size()) {
+        // If we find a repeating character...
+        while (count[str[second]]) {
+            // We move `first` forward and reset
+            // until we remove the repeated character.
             count[str[first]] = 0;
             first++;
         }
 
+        // Now we can include the new character safely.
         count[str[second]] = 1;
-        len = max(len, second - first +1);
-        
+
+        // Update maximum length if we got a bigger window
+        len = max(len, second - first + 1);
+
+        // Move forward
         second++;
     }
     
-    cout<<"max size substringg "<<len<<endl;
+    cout << "Max size of substring without repeating is " << len << endl;
 }
+
 int main()
 {
     /*
@@ -37,7 +43,7 @@ int main()
 
    */
 
-   string str = "abcdefbeadf";
+   string str = "abcdebf";
    SlidingWindow(str);
   return 0;
 }

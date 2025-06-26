6bebb0886dac944ca3280da47ebdf9c64feddce0
satyendragautam90 <satyendragautam901@gmail.com>
2025-06-26 10:19:10 +0530

    Insertion sort implementation using swapping and shifting technique

--- sortingTechnique/InsertionSort.cpp
+++ sortingTechnique/InsertionSort.cpp
@@ -14,7 +14,39 @@ Insertion sort(shifting) - It also works round by round . In first round ,start 
                 R4 as it is 
 */
 
+void InsertionSortUsingSwapping(vector<int>&ar){
+    cout<<"\nInsertion sort using swapping "<<endl;
+    for(int i = 1; i<ar.size(); i++){ // this loop run n-1 times
+        // check and swapp
+        for(int j = i; j>0; j--){
+            if(ar[j] < ar[j-1]){ // if current element is less than previous then swap.
+                swap(ar[j], ar[j-1]);
+            }
+            else{
+                break;
+            }
+        }
+    }
+}
 
+void InsertionSortUsingShifting(vector<int>&ar){
+    cout<<"\nInsertion sort using shifting "<<endl;
+
+    for(int i = 1; i<ar.size(); i++){ // this loop run n-1 times
+        int key = ar[i]; // to which we want to compare and place it after correct position.
+
+        int j = i-1; // start comparing from i-1 location. jaha i hai usse ek index piche se comare kro.
+
+        while (j>=0 && ar[j] > key) // jab tak key chhota hai tab tak shift krte rho and j must be greater than 0
+        {
+            ar[j + 1]  = ar[j]; // swap krna hai next element to with current j
+            j--;
+        }
+
+        ar[j+1] = key; // now j ya to 0 bn gya hota hai correct position se ek kam ho gya hoga.
+        
+    }
+}
 
 void PrintArray(vector<int>&ar){
     for(int i = 0; i<ar.size(); i++){
@@ -24,18 +56,12 @@ void PrintArray(vector<int>&ar){
 int main()
 {
     vector<int>arr{10, 8, 2, 3, 1, 4};
-    PrintArray(arr);
-
-
-    /*
-    
     cout << "Before sorting: ";
     PrintArray(arr);
 
     cout << "\nAfter sorting: ";
-    SelectionSort(arr);
+    InsertionSortUsingShifting(arr);
     PrintArray(arr);
-    */
 
     
 

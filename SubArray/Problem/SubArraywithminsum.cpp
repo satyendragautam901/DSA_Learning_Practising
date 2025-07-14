00aeb72d4e7b50819dc48d9845574ae328c1e8fc
satyendragautam90 <satyendragautam901@gmail.com>
2025-07-14 17:28:24 +0530

    sub array with min sum using stack

--- SubArray/Problem/SubArraywithminsum.cpp
+++ SubArray/Problem/SubArraywithminsum.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<stack>
 using namespace std;
 /*
 find the sum of min element of all the sub array.
@@ -21,9 +22,59 @@ int MinSubArraySum(vector<int>&ar){
 
     return sum;
 }
+
+int MinSubUsingStack(vector<int>&ar){
+    int n = ar.size();
+    vector<int>left(n), right(n);
+    stack<int>st;
+
+    // ple(previous left element)
+    for(int i = 0 ;i<n; i++){
+        while (!st.empty() && ar[st.top()] > ar[i])
+        {
+            st.pop();
+        }
+
+        left[i] = st.empty() ? i+1 : i-st.top();
+        st.push(i);
+        
+    }
+
+    // clear stack
+    while (!st.empty())
+    {
+        st.pop();
+    }
+
+    // nle(next left smaller)
+
+    for(int i = n-1; i>=0; i--){
+        while (!st.empty() && ar[st.top()] > ar[i])
+        {
+            st.pop();
+        }
+        right[i] = st.empty() ? n-i:st.top()-i;
+        st.push(i);
+    }
+
+    long long sum = 0;
+    int mod = 1e9 + 7; // used to avoid overflow 
+    for(int i = 0; i<n; i++){
+        sum = (sum + (1LL * ar[i] * left[i] * right[i]) % mod)%mod;
+    }
+
+    for(auto a: right){
+        cout<<a<<" ";
+    }
+    cout<<endl;
+    return sum;
+
+
+}
 int main()
 {
     vector<int>ar{3,1,2,4};
-    cout<<MinSubArraySum(ar);
+    cout<<MinSubArraySum(ar)<<endl;
+    cout<<MinSubUsingStack(ar)<<endl;
   return 0;
 }
